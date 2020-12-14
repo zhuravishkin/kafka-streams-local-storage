c2c246bda3a9bdd8546ec86317fe5f34284e4da7
alexey.zhuravishkin <zhuravishkin@gmail.com>
2020-12-14 11:06:23 +0300

    added KeyValueIterator

--- src/main/java/com/zhuravishkin/kafkastreamslocalstorage/service/KafkaStreamsTransformer.java
+++ src/main/java/com/zhuravishkin/kafkastreamslocalstorage/service/KafkaStreamsTransformer.java
@@ -1,10 +1,17 @@
 package com.zhuravishkin.kafkastreamslocalstorage.service;
 
 import com.zhuravishkin.kafkastreamslocalstorage.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.ValueTransformer;
 import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.processor.PunctuationType;
+import org.apache.kafka.streams.state.KeyValueIterator;
 import org.apache.kafka.streams.state.KeyValueStore;
 
+import java.time.Duration;
+
+@Slf4j
 public class KafkaStreamsTransformer implements ValueTransformer<User, User> {
     private KeyValueStore<String, User> stateStore;
     private final String storeName;
@@ -18,6 +25,13 @@ public class KafkaStreamsTransformer implements ValueTransformer<User, User> {
     public void init(ProcessorContext context) {
         this.context = context;
         stateStore = (KeyValueStore<String, User>) this.context.getStateStore(storeName);
+        context.schedule(Duration.ofSeconds(10), PunctuationType.WALL_CLOCK_TIME, timestamp -> {
+            KeyValueIterator<String, User> keyValueIterator = stateStore.all();
+            while (keyValueIterator.hasNext()) {
+                KeyValue<String, User> keyValue = keyValueIterator.next();
+                log.info("keyValue - " + keyValue.key + ":" + keyValue.value);
+            }
+        });
     }
 
     @Override
